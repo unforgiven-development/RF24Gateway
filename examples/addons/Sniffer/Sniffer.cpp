e16cf1ce36b5edc1280192242f49499f27397af0
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2016-09-04 03:13:20 -0500

    Sniffer.cpp: misc

--- examples/addons/Sniffer/Sniffer.cpp
+++ examples/addons/Sniffer/Sniffer.cpp
@@ -1,19 +1,19 @@
 
 /*
-   RF24Network Scanner by TMRh20 Aug 2016
-   Outputs data to console & UDP port 32001
-   See the LUA script for Wireshark at https://github.com/TMRh20/RF24Gateway/tree/master/examples/addons
-   
-   Impersonate any RF24Network/RF24Mesh/RF24Gateway node & scan traffic without disruption to the network
-*/
+ * RF24Network Scanner by TMRh20 Aug 2016
+ * Outputs data to console & UDP port 32001
+ * See the LUA script for Wireshark at https://github.com/TMRh20/RF24Gateway/tree/master/examples/addons
+ *
+ * Impersonate any RF24Network/RF24Mesh/RF24Gateway node & scan traffic without disruption to the network
+ */
 
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
-#include <RF24Mesh/RF24Mesh.h>  
+#include <RF24Mesh/RF24Mesh.h>
 #include <RF24Gateway/RF24Gateway.h>
 
 //RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ); 
-RF24 radio(25,0);
+RF24 radio(22,0);
 RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 RF24Gateway gw(radio,network,mesh);
@@ -24,22 +24,19 @@ int buffCtr = 0;
 uint32_t mesh_timer = 0;
 
 
-
 int main(int argc, char** argv) {
 
-  radio.begin();
-  radio.setAutoAck(0);
-  
-  // Because AutoAck is disabled, and we are not transmitting any payloads, we will not interfere with the network
-  // Setup this node as a clone of any network node to scan for all payloads passing to or through that node
-  gw.begin();
-  
-  
-  
- while(1){
-    
-  // Buffer all payloads while the radio has data  
-  while(radio.available()){
+	radio.begin();
+	radio.setAutoAck(0);
+
+	// Because AutoAck is disabled, and we are not transmitting any payloads, we will not interfere with the network
+	// Setup this node as a clone of any network node to scan for all payloads passing to or through that node
+	gw.begin();
+
+	while(1) {
+
+		// Buffer all payloads while the radio has data
+		while(radio.available()){
       sizes[buffCtr] = radio.getDynamicPayloadSize();
       radio.read(&buffer[buffCtr][0],sizes[buffCtr]);
       ++buffCtr;
